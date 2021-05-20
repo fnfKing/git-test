89d0afcb7578c705a0cfad0e327a93bf62a7ce13
fnfKing <fnfKing@fnf.com>
2021-05-20 21:16:02 +0800

    first commit

---
+++ src/main/java/com/fnf/git/GItHello.java
@@ -0,0 +1,11 @@
+package com.fnf.git;
+
+/**
+ * @Author： 冯南飞
+ * @Date： 2021/5/20 21:10
+ **/
+public class GItHello {
+    public static void main(String[] args) {
+        System.out.println("git hello");
+    }
+}
